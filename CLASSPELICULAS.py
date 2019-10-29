6a755ff9cacaaaee86180689f1fecc5c93e71ea4
almonacid98 <gabriel98almonacid@hotmail.com>
2019-10-29 12:22:07 -0300

    se creo la clase peliculas y se definio un constructor

---
+++ CLASSPELICULAS.py
@@ -0,0 +1,27 @@
+from CLASSAUTORES import*
+class Peliculas():
+    nombredepelicula = ''
+    tipodepelicula = ''
+    restriccionesdeedad = ''
+    quienhizolapelicula = Autores()
+
+    def __init__(self, nombredepelicula, tipodepelicula, restriccionesdeedad):
+        self.__nombredepelicula = nombredepelicula
+        self.__tipodepeliculas = tipodepelicula
+        self.__restriccionesdeedad = restriccionesdeedad
+
+    def getnombredepelicula(self):
+        return print(self.__nombredepelicula)
+
+    def gettipodepelicula(self):
+        return print(self.__tipodepeliculas)
+
+    def getrestriccionesdeedad(self):
+        return print(self.__restriccionesdeedad)
+
+
+print("Bienvenidos a la cartelera de información de cine")
+play = Peliculas("Avengers", "Ciencia ficción", "Apta para mayores de 13 años")
+play.getnombredepelicula()
+play.gettipodepelicula()
+play.getrestriccionesdeedad()
