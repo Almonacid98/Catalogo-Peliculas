6483f91164bb6e25f145174ae3debb78571fb240
almonacid98 <gabriel98almonacid@hotmail.com>
2019-10-30 13:13:11 -0300

    Se modifico el menu, esta en proceso de cambios

--- MENU.py
+++ MENU.py
@@ -1,39 +1,19 @@
 import sys
 from CLASSPELICULAS import Peliculas
 class Menu():
-    #Creacion de menu mediante un constructor
-    def __init__(self):
-        self.__peliculas = Peliculas()
-        self.__selection = {
-                            "1": self.__peliculas.getnombredepelicula(),
-                            "2": self.__peliculas.gettipodepelicula(),
-                            "3": self.__peliculas.getrestriccionesdeedad()
-        }
+        def selectmenu(self, nombredepelicula, tipodepelicula, restriccionesdeedad):
+            self.__peliculas = Peliculas(nombredepelicula, tipodepelicula, restriccionesdeedad)
+            print("BIENVENIDOS AL CATALOGO DE PELICULAS!!! DONDE SIEMPRE SE INFORMA SOBRE CADA PELICULA QUE SALE A CARTELERAS! ")
+            print("1 = VER CATALOGO DE PELICULAS DISPONIBLES")
+            print("2 = VER PELICULAS CLASIFICADAS SEGUN TIPO DE GENERO")
+            print("3 = ELECCION DE PELICULAS SEGUN RESTRICCION DE EDAD")
 
-    def vermenu(self):
-        print(""""
-     BIENVENIDOS AL CATALOGO DE PELICULAS!!! DONDE SIEMPRE SE INFORMA SOBRE CADA PELICULA QUE SALE A CARTELERAS!
-     PRESIONE LAS SIGUIENTES OPCIONES PARA PODER EMPEZAR:
-     1 = CATALOGO DE INFORMACION DE PELICULAS DISPONIBLES
-     2 = PELICULAS POR EL TIPO DE GENERO QUE LE GUSTEN 
-     3 = ELECCION DE PELICULAS SEGUN RESTRICCIONES DE EDAD
-     
-        
-  
-        """)
 
-    def ejecuciondemenu(self):
-        #muestra el menu y responde a lo que la persona requiere
-        while True:
-            self.vermenu()
-            seleccionaropcion = input("INGRESA UNA OPCION PARA COMENZAR: ")
-            accionseleccion = self.__selection.get(seleccionaropcion)
-            if accionseleccion:
-                accionseleccion()
-            else:
-                print("LA OPCION INGRESADA NO ES UNA OPCION VALIDA".format(seleccionaropcion))
-    def mostrarinformacion(self, nombre = None):
-        if not nombre:
-             nombre = self.__peliculas.getnombredepelicula()
-        for nombrepelicula in nombre:
-            print("[0]: [1]\n[2]".format(nombrepelicula))
+            #LEER OPCION DE USUARIO
+            choice = input("PRESIONE LAS SIGUIENTES OPCIONES PARA PODER EMPEZAR[1/2/3]  = ")
+
+            if choice == "1":
+                print(self.__peliculas.nombredepelicula())
+
+
+
