ab5f288dbfa0ac83f04b2169402923d48c1f8283
almonacid98 <gabriel98almonacid@hotmail.com>
2019-10-30 13:10:46 -0300

    se definio un constructor y se avanzo en la clase

--- CLASSDIRECTORES.py
+++ CLASSDIRECTORES.py
@@ -5,5 +5,18 @@ class Directores():
     cantidaddepelicualas = ''
     añosdeexperiencia = ''
 
+    def __init__(self, namedirector, peliculaquerealizo, edad, cantidaddepeliculas, añosdeexperiencia):
+        self.__nombredirector = namedirector
+        self.__peliculaquerealizo = peliculaquerealizo
+        self.__edad = edad
+        self.__cantidaddepeliculas = cantidaddepeliculas
+        self.__añosdeexperiecias = añosdeexperiencia
 
+    def setnombredirector(self, nombre):
+        self.__nombredirector = nombre
 
+    def setpeliculashechas(self, quehizo):
+        self.__peliculaquerealizo = quehizo
+
+    def setedad(self, edad):
+        self.__edad = edad
