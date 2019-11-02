17bb1dc88cbed9b1cd729b781fdaf90dd515d74d
almonacid98 <gabriel98almonacid@hotmail.com>
2019-11-02 13:54:22 -0300

    se agrego una nueva clase que contiene sinopsis, todavia esta en proceso

---
+++ CLASSINFORMACIONPELICULAS.py
@@ -0,0 +1,16 @@
+class Sinopsis():
+    informacion = ''
+
+
+
+    def verinformacionavengers(self):
+        for lista in informacion:
+            print(lista)
+
+
+
+
+informacion = Sinopsis()
+informacion.date = open('Sinopsis Avengers.txt', 'r')
+informacion.date.readlines()
+informacion.verinformacionavengers()
