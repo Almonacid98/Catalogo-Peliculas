73e075d5e2f0007a903e035dcae90211d40aac73
almonacid98 <gabriel98almonacid@hotmail.com>
2019-11-02 13:51:01 -0300

    El menu es funcional, se puede pedir informacion de clase catalogos

--- CLASECATALOGO.py
+++ CLASECATALOGO.py
@@ -1,42 +1,53 @@
-
 from CLASSPELICULAS import *
 class Catalogo():
-    catalogo = []
-    def verPeliculas(self, catalogo):
-        for a in catalogo:
+    catalogolist = []
+    def verPeliculas(self):
+        for a in self.catalogolist:
             print("\nNombre:", a.getnombredepelicula(),
-                  "\n Tipo:", a.gettipodepelicula(), "\n Restricción:", a.getrestriccionesdeedad(),
-                  "\n Duración:", a.getduraciondepelicula)
+                  "\n Género:", a.gettipodepelicula(), "\n Restricción:", a.getrestriccionesdeedad(),
+                  "\n Duración:", a.getduraciondepelicula())
 
     def crearPelicula(self):
-        pelis = Peliculas()
-        self.catalogo.append(pelis)
+        pelis = Peliculas("The Avengers: Los Vengadores", "Acción | Aventura | Ciencia ficción", "Apta para mayores de 12", "2h 23m")
+        pelis1 = Peliculas("Deadpool", "Acción | Aventura | Comedia", "Apta para mayores de 18", "1h 49m")
+        #pelis2 = Peliculas()
+        self.catalogolist.append(pelis)
+        self.catalogolist.append(pelis1)
+        #self.catalogolist.append(pelis2)
 
     def popborrarPeliculas(self):
-        return self.catalogo.pop()
+        return self.catalogolist.pop()
 
-    def menu(self):
+    def menuinicio(self):
         while True:
             print(
                 "¡CATALOGO DE PELICULAS!  " "\n ¡BIENVENIDOS! AQUI SE PODRA INFORMAR SOBRE PELICULAS, PARA ELEGIR O RECOMENDAR A OTROS USUARIOS")
             print("1 = VER CATALOGO DE PELICULAS DISPONIBLES")
-            print("2 = AGREGAR PELICULAS")
+            print("2 = VER INFORMACION DE DIRECTORES")
             print("3 = BORRAR PELICULAS")
             print("4 = EXIT")
-            choice = input("PRESIONE LAS SIGUIENTES OPCIONES PARA PODER EMPEZAR[1/2/3]  = ")
+            choice = input("PRESIONE LAS SIGUIENTES OPCIONES PARA PODER EMPEZAR[1/2/3/4]  = ")
             if choice == '1':
-                catalogos.verPeliculas()
-                play.menu2()
-            elif choice == '2':
                 catalogos.crearPelicula()
+                catalogos.verPeliculas()
+                while True:
+                    choice = input("¿DESEA VER SINOPSIS DE ALGUNA PELICULA? [si/no]")
+                    if choice == 'si':
+                        print("Ingrese el nombre de la pelicula:")
+
+                    else:
+                        print("El nombre de la pelicula no es válido, o no existe en nuestra lista de catalogos")
+
+            elif choice == '2':
+                pass
             elif choice == '3':
                 catalogos.popborrarPeliculas()
             elif choice == '4':
                 print(
-                    "GRACIAS POR UTILIZAR NUESTRO SERVICIO.... ESPERAMOS QUE SE HAYA PODIDO INFORMAR... HASTA LA PRÓXIMA!!")
+                    "GRACIAS POR UTILIZAR NUESTRO SERVICIO.... ESPERAMOS QUE SE HA1YA PODIDO INFORMAR... HASTA LA PRÓXIMA!!")
                 break
             else:
                 print("\n OPCION INCORRECTA.... VUELVA A ELEGIR UNA OPCION EXISTENTE EN EL SISTEMA....")
 
 catalogos = Catalogo()
-catalogos.menu()
+catalogos.menuinicio()
