e157638a5678a60a595c57a0a89c5a741adb315b
almonacid98 <gabriel98almonacid@hotmail.com>
2019-11-03 23:03:36 -0300

    se arreglo codigo final listo

--- CLASECATALOGO.py
+++ CLASECATALOGO.py
@@ -26,23 +26,29 @@ class Catalogo():
 
     def borrarPeliculadelista(self):
         self.getverPeliculas()
-        self.__catalogolist.remove(input("Ingrese la pelicula que desea eliminar:"))
+        del(self.__catalogolist[int(input("Elimine por indice lo que desee borrar:"))])
 
 
     def getverPeliculas(self):
+        contador = 0
         for a in self.__catalogolist:
+            contador = contador + 1
+            print("\n", contador)
             print("\nNombre:", a.getnombredepelicula(),
                   "\n Género:", a.gettipodepelicula(), "\n Restricción:", a.getrestriccionesdeedad(),
                   "\n Duración:", a.getduraciondepelicula())
 
 
     def getverdirectores(self):
+        contador = 0
         for a in self.__Directoreslist:
+            contador = contador + 1
+            print("\n", contador)
             print("\nNombre del director:", a.getnombredirector(),
                   "\n Pelicula que realizó:", a.getpeliculashechas(), "\n Edad:", a.getedad(),
                   "\n Cantidad de peliculas:", a.getpeliculascantidad(), "\n Años de experiencia:", a.getexperiencia())
 
-
+        input("Presiona enter para volver al menu.......")
     def getcatalogo(self):
         return self.__catalogolist
 
